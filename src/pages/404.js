122579e67adc08531e05215f11283a401381d271
sushant-dasuri <sushant.dasuri@more.global>
2023-08-25 00:03:53 +0530

    Ading styles to the project

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Layout from '../components/Layout';
 
 export default function NotFound() {
     return (
-        <div>
+       <Layout>
+          <div>
         <h2>404</h2>
         <p>Sorry, that page doesn't exist (yet)!</p>
       </div>
+       </Layout>
     )
 }
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Layout from '../../components/Layout';
+import * as styles from '../../styles/projects.module.css'
 
 export default function Projects() {
     return (
-        <div>
+       <Layout>
+          <div className={styles.portfolio}>
         <h2>Portfolio</h2>
         <h3>Projects & Websites I've Created</h3>
       </div>
+       </Layout>
     )
 }
