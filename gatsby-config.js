0df9270926c2046261f874ca0cb0951c90531ac8
sushant-dasuri <sushant.dasuri@more.global>
2023-08-26 19:38:37 +0530

    Source plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,25 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
+  plugins: [ {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      // The unique name for each instance
+      name: `notes`,
+      // Path to the directory
+      path: `${__dirname}/src/notes/`,
+    },
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      // The unique name for each instance
+      name: `projects`,
+      // Path to the directory
+      path: `${__dirname}/src/projects/`,
+    },
+  },
+],
   siteMetadata: {
     title: 'Web Warrior',
     description: 'web dev portfolio',
