2896f6b9460d4ecf2ff5b5eec2bb6f8d08888b6f
sushant-dasuri <sushant.dasuri@more.global>
2023-08-27 19:22:40 +0530

    Template Components

---
+++ src/templates/project-details.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Layout from "../components/Layout"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import * as styles from '../styles/project-details.module.css'
+
+
+export default function ProjectDetails() {
+    return (
+        <Layout>
+            <div className={styles.details}>
+                <h2>title</h2>
+                <h3>stack</h3>
+                <div className={styles.featured}>
+
+                </div>
+                <div className={styles.html} />
+            </div>
+        </Layout>
+    )
+}
